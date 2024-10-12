0badd2cb7965de503a2cc0db1f2d1a8d66371b2a
Reagan <reaganjose2005@gmail.com>
2024-10-12 13:41:52 -0500

    test

--- Django Tutorial/students/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class CreateStudentForm(forms.Form):
-    first_name = forms.CharField(max_length=255, label="First Name")
-    last_name = forms.CharField(max_length=255, label="Last Name")
-    email = forms.EmailField(label="Email")
-
-
--- Django Tutorial/students/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Student(models.Model):
-
-    first_name = models.CharField(max_length=255, null = True)
-    last_name = models.CharField(max_length=255, null = True)
-    email = models.EmailField(null=True)
