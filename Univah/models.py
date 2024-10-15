166b080a486f02d5d763c84ffaeb0d1ec4abad38
Reagan <reaganjose2005@gmail.com>
2024-10-15 16:26:52 -0500

    new

--- Univah/models.py
+++ Univah/models.py
@@ -8,8 +8,9 @@ class Student(models.Model):
     email = models.EmailField(null=True)
 
 
-class TechStudent(models.Model):
+class Rider(models.Model):
 
     first_name = models.CharField(max_length=255, null = True)
+    last_name = models.CharField(max_length=255, null = True)
     CWID = models.CharField(max_length=9, null = True)
     email = models.EmailField(null=True)
