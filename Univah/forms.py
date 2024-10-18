bb69396f50dbffb47a6794093f56f45331a1f852
Reagan <reaganjose2005@gmail.com>
2024-10-17 22:04:18 -0500

    Deleted Tutorial stuff

--- Univah/forms.py
+++ Univah/forms.py
@@ -1,14 +1,6 @@
 from django import forms
 
 
-class CreateStudentForm(forms.Form):
-    first_name = forms.CharField(max_length=255, label="First Name")
-    last_name = forms.CharField(max_length=255, label="Last Name")
-    email = forms.EmailField(label="Email")
-
-
-
-
 class SigninForm(forms.Form):
     CWID = forms.CharField(max_length=9, label="CWID")
 
--- Univah/models.py
+++ Univah/models.py
@@ -1,12 +1,5 @@
 from django.db import models
 
-# Create your models here.
-class Student(models.Model):
-
-    first_name = models.CharField(max_length=255, null = True)
-    last_name = models.CharField(max_length=255, null = True)
-    email = models.EmailField(null=True)
-
 class Rider(models.Model):
 
     first_name = models.CharField(max_length=255, null = True)
