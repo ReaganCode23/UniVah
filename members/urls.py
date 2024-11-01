5610c4644c9730ffc4d6b6cb8e2f7b6366668aca
ReaganCode23 <reaganjose2005@gmail.com>
2024-10-31 22:27:48 -0500

    Fixed Logout on register page

--- members/urls.py
+++ members/urls.py
@@ -5,7 +5,7 @@ from .views import UserRegisterView
 urlpatterns = [
 
     path('login_user', views.login_user, name = "login"),
-    path('logout_user', views.logout_user, name ='logout'),
+    path('logout_user', views.logout_user, name ="logout"),
     path('register/', UserRegisterView.as_view(), name = 'register'),
 
 ]
--- members/views.py
+++ members/views.py
@@ -30,4 +30,4 @@ def login_user(request):
 
 def logout_user(request):
     logout(request)
-    return redirect('home')
+    return redirect('transpo')
